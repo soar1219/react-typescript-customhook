a5ffa1055804e7f33475e89c28dbe9f79992a793
soar1219 <tsubasa1219.info@gmail.com>
2023-10-12 00:41:55 +0900

    カスタムフックの作成

---
+++ src/hooks/UseAllUsers.ts
@@ -0,0 +1,34 @@
+/* 全ユーザー一覧を取得するカスタムフック */
+
+import { useState } from "react";
+import { UserProfile } from "../types/UserPlofile";
+import axios from "axios";
+import { User } from "../types/api/user";
+
+
+
+export const UseAllUsers = () => {
+
+  const [userPlofiles , setUserPlofiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    setError(false);
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res)=> {
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}${user.username}`,
+        email: user.email,
+        address: `${user.address.city}${user.address.suite}${user.address.street}`
+      }));
+      setUserPlofiles(data);
+    }).catch(() => {
+      setError(true);
+    }).finally(() => {
+      setLoading(false);
+    });
+  }
+  return { getUsers , userPlofiles , loading , error }
+}
