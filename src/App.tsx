809cdde966efaaa07943d173b9188cc280a98076
soar1219 <tsubasa1219.info@gmail.com>
2023-10-12 00:17:39 +0900

    カスタムフックを使わないデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { UserCard } from './components/UserCard';
+import { User } from './types/api/user';
+import { useState } from 'react';
+import { UserProfile } from './types/UserPlofile';
 
 function App() {
+  const [userPlofiles , setUserPlofiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchData = () => {
+    setLoading(true);
+    setError(false);
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res)=> {
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}${user.username}`,
+        email: user.email,
+        address: `${user.address.city}${user.address.suite}${user.address.street}`
+      }));
+      setUserPlofiles(data);
+    }).catch(() => {
+      setError(true);
+    }).finally(() => {
+      setLoading(false);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickFetchData}>データを取得</button>
+      <br />
+      {error ? (
+        <p style ={{color: "red"}}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : <>
+            {userPlofiles.map((user)=> (
+              <UserCard key={user.id} user={user}  />
+            ))}
+          </> }
     </div>
   );
 }
