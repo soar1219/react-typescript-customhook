7a54c37ba802401ed2737839d86e441b84d2eec5
soar1219 <tsubasa1219.info@gmail.com>
2023-10-12 00:47:48 +0900

    カスタムフックの呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import axios from 'axios';
 import './App.css';
 import { UserCard } from './components/UserCard';
-import { User } from './types/api/user';
-import { useState } from 'react';
-import { UserProfile } from './types/UserPlofile';
+import { UseAllUsers } from './hooks/UseAllUsers';
 
 function App() {
-  const [userPlofiles , setUserPlofiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+
+  const { getUsers , userPlofiles , loading , error } = UseAllUsers();
 
   const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res)=> {
-      const data = res.data.map((user) => ({
-        id: user.id,
-        name: `${user.name}${user.username}`,
-        email: user.email,
-        address: `${user.address.city}${user.address.suite}${user.address.street}`
-      }));
-      setUserPlofiles(data);
-    }).catch(() => {
-      setError(true);
-    }).finally(() => {
-      setLoading(false);
-    });
+    getUsers();
   }
   return (
     <div className="App">
